781d1ae05be894beb599e46a425aa3383c5d8219
José Felipe <132314205+fel1pee3@users.noreply.github.com>
2024-03-01 20:52:53 -0300

    scroll feito

---
+++ scripts/scroll.js
@@ -0,0 +1,15 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal(".section-scroll", {
+    duration: 900,
+    delay: 200,
+    easing: "ease-in",
+});
+sr.reveal(".caixa-scroll", {
+    duration: 900,
+    delay: 400,
+    distance: "100px",
+    easing: "ease-in",
+    origin: 'left'
+});
+
