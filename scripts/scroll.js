92124d96ccc1f31108a86257b72b9ee77c064260
José Felipe <132314205+fel1pee3@users.noreply.github.com>
2024-03-02 20:10:11 -0300

    scrolls melhorados

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,15 +1,25 @@
 window.sr = ScrollReveal({reset: true});
 
-sr.reveal(".section-scroll", {
-    duration: 900,
-    delay: 180,
-    easing: "ease-in",
-});
 sr.reveal(".caixa-scroll", {
-    duration: 900,
+    duration: 800,
     delay: 300,
     distance: "100px",
-    easing: "ease-in",
+    easing: "ease",
     origin: 'left'
+});
+
+sr.reveal(".header-scroll", {
+    duration: 800,
+    delay: 300,
+    distance: "100px",
+    easing: "ease",
+    origin: 'top'
+});
+
+sr.reveal(".caixa-scroll-surgi", {
+    duration: 800,
+    delay: 300, 
+    easing: "ease",
+    scale: 0.85 
 });
 
